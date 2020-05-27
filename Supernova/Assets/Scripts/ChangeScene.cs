44697b8ecd031100e3065089b2d6673f395fb5cf
DanielFS7 <daniel.sanchezramirez02@gmail.com>
2020-05-27 17:37:15 -0500

    ...

---
+++ Supernova/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+
+public class ChangeScene : MonoBehaviour
+{
+    public bool FirstScene = false;
+    public VideoPlayer video;
+   public void changeScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+    private void Start()
+    {
+        if(FirstScene)
+        {
+            float time = (float)video.clip.length;
+            StartCoroutine(LoadNextScene(time));
+        }
+    }
+
+    IEnumerator LoadNextScene(float time)
+    {
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Supernova/Assets/Scripts/GPS.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GPS : MonoBehaviour
+{
+    string urlMap = "";
+    public RawImage imageMap;
+    public Text latitudeText;
+    public Text longitudeText;
+    public int zoom = 20;
+
+    private void Start()
+    {
+        StartCoroutine("GetMap");
+    }
+    public void ZoomIn()
+    {
+        zoom++;
+        StartCoroutine("GetMap");
+    }
+    public void ZoomOut()
+    {
+        if(zoom>=0)
+        {
+            zoom--;
+        }
+        StartCoroutine("GetMap");
+    }
+    IEnumerator GetMap()
+    {
+        Input.location.Start();
+        float latitud = Input.location.lastData.latitude;
+        yield return latitud;
+        float longitud = Input.location.lastData.longitude;
+        yield return longitud;
+
+        urlMap = "http://maps.google.com/maps/api/staticmap?center=" + latitud + "," + longitud + "&markers=color:red%7Clabel:S%" + latitud + "," + longitud + "&zoom=" + zoom + "&size=512x512" + "&maptype=hybrid&markers=color:red|label:P|" + latitud + "," + longitud + "&type=hybrid&sensor=true?a.jpg";
+
+        WWW www = new WWW(urlMap);
+        yield return www;
+
+        imageMap.texture = www.texture;
+        latitudeText.text = "" + latitud;
+        longitudeText.text = "" + longitud;
+    }
+}
